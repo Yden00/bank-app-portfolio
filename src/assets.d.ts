f0452a34684fbdae575fd3b14d411eb19a51b520
Uladzislau <vladtett2000@gmail.com>
2023-07-26 19:32:18 +0200

    Try to deploy xd

--- src/assets.d.ts
+++ src/assets.d.ts
@@ -1,14 +1,42 @@
-declare module "*.jpg" {
-    const value: string;
-    export default value;
-  }
-  
-  declare module "*.png" {
-    const value: string;
-    export default value;
-  }
-  
-  declare module "*.svg" {
-    const content: string;
-    export default content;
-  }
+
+
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.svg";
+
+declare module "assets" {
+  const people01: string;
+  const people02: string;
+  const people03: string;
+  const facebook: string;
+  const instagram: string;
+  const linkedin: string;
+  const twitter: string;
+  const airbnb: string;
+  const binance: string;
+  const coinbase: string;
+  const dropbox: string;
+  const send: string;
+  const shield: string;
+  const star: string;
+
+  const assetsArray: string[];
+
+  const assets: {
+    people01: string;
+    people02: string;
+    people03: string;
+    facebook: string;
+    instagram: string;
+    linkedin: string;
+    twitter: string;
+    airbnb: string;
+    binance: string;
+    coinbase: string;
+    dropbox: string;
+    send: string;
+    shield: string;
+    star: string;
+  };
+
+}
